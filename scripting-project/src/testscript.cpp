aee8bcd58a3b004b76219d11b43d0cd3b8494480
Wen Loong <wenloong@portylstudios.com>
2024-12-13 19:47:00 +0800

    further splitting into dll (engine) and exe

--- scripting-project/src/testscript.cpp
+++ scripting-project/src/testscript.cpp
@@ -6,20 +6,10 @@
 
 extern "C" void __FLX_API Start()
 {
-  #if 0
-  int* object_a = new int(10);
-  IntManager::SetActiveObject(object_a);
-  #else
   IntManager::SetActiveObject(IntManager::CreateObject("object_a"));
   IntManager::ChangeValue(10);
-  #endif
 
   std::cout << "Active object: " << *IntManager::GetActiveObject() << std::endl;
 
-  #if 0
-  delete object_a;
-  object_a = nullptr;
-  #else
   IntManager::DestroyObject("object_a");
-  #endif
 }
